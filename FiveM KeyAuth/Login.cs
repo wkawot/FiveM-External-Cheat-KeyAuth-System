8518aa9fda6bf64039137766b430a0ff9d82dfe8
sassteermsblank881 <hnissaste@outlook.com>
2025-09-18 19:14:19 +0400

    Login.cs

---
+++ FiveM KeyAuth/Login.cs
@@ -0,0 +1,183 @@
+using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+//chres0
+namespace KeyAuth
+{
+    public partial class Login : Form
+    {
+
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr CreateRoundRectRgn(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        public static api KeyAuthApp = new api(
+           name: "FiveM1",
+           ownerid: "PwBTV43ofz",
+           secret: "226b407f71d9c16679b3c42401bcb21a2f651375df726c7f247b6b325fd21272",
+           version: "1.0"
+       );
+
+        private void ShowResponse(string type)
+        {
+            MessageBox.Show($"It took {api.responseTime} msg to {type}");
+        }
+
+        public Login()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
+
+            string savedUsername = GetSavedUsernameFromRegistry();
+            string savedPassword = GetSavedPasswordFromRegistry();
+            if (!string.IsNullOrEmpty(savedUsername) && !string.IsNullOrEmpty(savedPassword))
+            {
+                username.Text = savedUsername;
+                password.Text = savedPassword;
+            }
+        }
+
+        private string GetSavedUsernameFromRegistry()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\FiveMExternal"))
+            {
+                if (key != null)
+                {
+                    return key.GetValue("Username") as string;
+                }
+            }
+            return null;
+        }
+
+        private void SaveCredentialsToRegistry(string username, string password)
+        {
+            using (RegistryKey key = Registry.CurrentUser.CreateSubKey(@"Software\FiveMExternal"))
+            {
+                if (key != null)
+                {
+                    key.SetValue("Username", username);
+                    key.SetValue("Password", password);
+                }
+            }
+        }
+
+        private string GetSavedPasswordFromRegistry()
+        {
+            using (RegistryKey key = Registry.CurrentUser.OpenSubKey(@"Software\FiveMExternal"))
+            {
+                if (key != null)
+                {
+                    return key.GetValue("Password") as string;
+                }
+            }
+            return null;
+        }
+
+        private void siticoneControlBox1_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        
+        public static bool SubExist(string name)
+        {
+            if(KeyAuthApp.user_data.subscriptions.Exists(x => x.subscription == name))
+                return true;
+            return false;
+        }
+        
+        private void Login_Load(object sender, EventArgs e)
+        {
+            KeyAuthApp.init();
+
+            if (KeyAuthApp.response.message == "invalidver")
+            {
+                if (!string.IsNullOrEmpty(KeyAuthApp.app_data.downloadLink))
+                {
+                    DialogResult dialogResult = MessageBox.Show("Yes to open file in browser\nNo to download file automatically", "Auto update", MessageBoxButtons.YesNo);
+                    switch (dialogResult)
+                    {
+                        case DialogResult.Yes:
+                            Process.Start(KeyAuthApp.app_data.downloadLink);
+                            Environment.Exit(0);
+                            break;
+                        case DialogResult.No:
+                            WebClient webClient = new WebClient();
+                            string destFile = Application.ExecutablePath;
+
+                            string rand = random_string();
+
+                            destFile = destFile.Replace(".exe", $"-{rand}.exe");
+                            webClient.DownloadFile(KeyAuthApp.app_data.downloadLink, destFile);
+
+                            Process.Start(destFile);
+                            Process.Start(new ProcessStartInfo()
+                            {
+                                Arguments = "/C choice /C Y /N /D Y /T 3 & Del \"" + Application.ExecutablePath + "\"",
+                                WindowStyle = ProcessWindowStyle.Hidden,
+                                CreateNoWindow = true,
+                                FileName = "cmd.exe"
+                            });
+                            Environment.Exit(0);
+
+                            break;
+                        default:
+                            MessageBox.Show("Invalid option");
+                            Environment.Exit(0);
+                            break;
+                    }
+                }
+                MessageBox.Show("Version of this program does not match the one online. Furthermore, the download link online isn't set. You will need to manually obtain the download link from the developer");
+                Environment.Exit(0);
+            }
+            
+            if (!KeyAuthApp.response.success)
+            {
+                MessageBox.Show(KeyAuthApp.response.message);
+                Environment.Exit(0);
+            }
+        }
+
+        static string random_string()
+        {
+            string str = null;
+
+            Random random = new Random();
+            for (int i = 0; i < 5; i++)
+            {
+                str += Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65))).ToString();
+            }
+            return str;
+
+        }
+
+
+        private void LoginBtn_Click(object sender, EventArgs e)
+        {
+
+            KeyAuthApp.login(username.Text,password.Text);
+            if (KeyAuthApp.response.success)
+            {
+                SaveCredentialsToRegistry(username.Text, password.Text);
+                Main main = new Main();
+                main.Show();
+                this.Hide();
+            }
+            else
+                status.Text = "Status: " + KeyAuthApp.response.message;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            MessageBox.Show("fivem", "Password", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+        }
+    }
+}
